05381b8c0544f48fb562f9b0fa9c3471f8d960e5
yev <josephharveyangeles@gmail.com>
2018-04-15 15:46:13 +0800

    Initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,111 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="grey-10"
+        :inverted="$q.theme === 'ios'"
+      >
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title v-if="!leftDrawerOpen" class="text-center">
+          JiKaBo Catering Services
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      content-class="bg-grey-10"
+    >
+    <div class="q-py-lg text-center">
+      <img alt="Jikabo logo" src="~assets/logo.png">
+    </div>
+    <div class="row">
+      <div class="col-9">
+        <q-list no-border>
+          <q-item>
+            <q-item-main>
+              <div class="nav q-display-1 right text-brown-6">Home</div>
+            </q-item-main>
+          </q-item>
+          <q-item>
+            <q-item-main>
+              <div class="nav q-display-1 right text-brown-6">Gallery</div>
+            </q-item-main>
+          </q-item>
+          <q-item>
+            <q-item-main>
+              <div class="nav q-display-1 right text-brown-6">About Us</div>
+            </q-item-main>
+          </q-item>
+          <q-item>
+            <q-item-main>
+              <div class="nav q-display-1 right text-brown-6">Contacts</div>
+            </q-item-main>
+          </q-item>
+        </q-list>
+      </div>
+      <div class="col-3">
+          <q-list
+          no-border
+          inset-delimiter
+        >
+          <q-item class="package-link">
+            <q-btn square icon="child care" color="red-10" size="xl" class="full-width"/>
+          </q-item>
+          <q-item class="package-link">
+            <q-btn square icon="spa" color="deep-orange-10" size="xl" class="full-width"/>
+          </q-item>
+          <q-item class="package-link">
+            <q-btn square icon="cake" color="red" size="xl" class="full-width"/>
+          </q-item>
+          <q-item class="package-link">
+            <q-btn square icon="whatshot" color="deep-orange" size="xl" class="full-width"/>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'LayoutDefault',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.q-layout-header
+  box-shadow 0
+.right
+  text-align right
+.package-link
+  padding 0
+  button
+    border-radius 0
+.nav
+  cursor pointer
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="q-pa-xl q-mt-xl">
+      <p class="header-quote text-italic q-display-3 text-white float-right">
+        “Pull up a chair. Take a taste. Come join us. Life is so endlessly delicious.”
+      </p>
+  </q-page>
+</template>
+<style lang="stylus" scoped>
+.header-quote
+  max-width 700px
+  font-family 'Helvetica Neue'
+  text-align right
+</style>
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
